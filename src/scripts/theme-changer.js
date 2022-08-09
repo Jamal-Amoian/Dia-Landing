a248b430c040deb7a3eb2f88d932a11f1c48ea31
Jamal Amoian <leonidagutin09@gmail.com>
2022-08-09 14:11:22 +0300

    make build

--- src/scripts/theme-changer.js
+++
@@ -1,8 +0,0 @@
-
-const themeButton = document.querySelector('.theme-changer');
-const header = document.querySelector('.header');
-
-themeButton.onclick = () => {
-  header.classList.toggle('dark-theme');
-  header.classList.toggle('light-theme');
-};
